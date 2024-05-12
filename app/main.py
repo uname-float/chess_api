14e1d6c718219bd41b8448f82ff3f2b4bcdb8f3c
revego <revego@code4you.cloud>
2024-05-12 09:21:14 +0000

    update endpoints

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import models, crud
+
 from fastapi import FastAPI, Depends
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
@@ -6,10 +8,7 @@ from sqlalchemy.orm import Session
 from typing import List
 
 from database import SessionLocal, engine
-
-#from app.routers import chess_event
-from app import schemas, models, crud
-from app.schemas import Calendar
+from schemas import Calendar
 
 app = FastAPI()
 
@@ -26,14 +25,14 @@ async def root():
     return {"message": "Welcome to the Chess Events API!"}
 
 # Definisci l'endpoint API per ottenere gli eventi di scacchi
-@app.get("/events/", response_model=list[schemas.Calendar])
+@app.get("/events/", response_model=list[Calendar])
 def read_chess_events(db: Session = Depends(get_db)):
     ##-bad#return chess_event.get_chess_events(db)
     events = jsonable_encoder(db.query(models.Calendar).all())
     return JSONResponse(content=events)
 
 # Definisci l'endpoint API per ottenere graduatoria/ELO di scacchi
-@app.get("/ratings/", response_model=list[schemas.Calendar])
+@app.get("/ratings/", response_model=list[Calendar])
 def read_chess_events(db: Session = Depends(get_db)):
     ##-bad#return chess_event.get_chess_events(db)
     events = jsonable_encoder(db.query(models.Calendar).all())
